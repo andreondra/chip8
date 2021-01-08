421b3921d0f19a848be510f9373bfd8960272cd4
Ondrej Golasowski <golasowski.o@gmail.com>
2021-01-08 22:52:40 +0100

    Chip8 project skeleton implemented.

---
+++ src/Chip8.cpp
@@ -0,0 +1,101 @@
+#include "Chip8.hpp"
+#include <cstdio>
+#include <cstring>
+#include <chrono>
+
+const uint8_t Chip8::font[SIZE_FONT] = {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+
+Chip8::Chip8() : randGenerator(std::chrono::system_clock::now().time_since_epoch().count()){
+
+    memcpy(memory, font, sizeof(uint8_t) * SIZE_FONT);
+    randByte = std::uniform_int_distribution<uint8_t>(0, 255);
+}
+
+Chip8::returnStatus_t Chip8::loadROM(const char *filename){
+
+    FILE *rom;
+    size_t readStatus;
+    size_t dataSize;
+
+    if(NULL == (rom = fopen(filename, "rb"))){
+        return RS_ROM_IOERROR;
+    }
+
+    readStatus = fread(memory + ADR_START, sizeof(uint8_t), 4096, rom);
+
+    if(readStatus != 4096){
+        return RS_ROM_IOERROR;
+    }
+
+    fclose(rom);
+    rom = NULL;
+    
+    return RS_OK;
+}
+
+void Chip8::OP_00E0(){
+    memset(display, 0, SIZE_SCREEN);
+}
+
+void Chip8::OP_00EE(){
+    pc = stack[--stackPointer];
+}
+
+void Chip8::OP_1NNN(){
+    pc = opcode & 0x0FFF;
+}
+
+void Chip8::OP_2NNN(){
+
+    stack[stackPointer] = pc;
+    stackPointer++;
+    pc = opcode & 0x0FFF;
+}
+
+void Chip8::OP_3XKK(){
+
+    uint8_t Vx = (opcode & 0x0F00) >> 8;
+    uint8_t byte = opcode & 0x00FF;
+
+    if(registers[Vx] == byte)
+        pc += 2;
+}
+
+void Chip8::OP_4XKK(){
+
+    uint8_t Vx = (opcode & 0x0F00) >> 8;
+    uint8_t byte = opcode & 0x00FF;
+
+    if(registers[Vx] != byte)
+        pc += 2;
+}
+
+void Chip8::OP_5XY0(){
+
+    uint8_t Vx = (opcode & 0x0F00) >> 8;
+    uint8_t Vy = (opcode & 0x00F0) >> 4;
+
+    if(registers[Vx] == registers[Vy])
+        pc += 2;
+}
+
+void Chip8::OP_6XKK(){
+    
+}
---
+++ src/Chip8.hpp
@@ -0,0 +1,57 @@
+#ifndef __GSS_CHIP8_H__
+#define __GSS_CHIP8_H__
+
+#include <cstdint>
+#include <random>
+
+/**
+ * CHIP-8 interpreter.
+ * @author Ondrej Golasowski
+ */
+class Chip8{
+
+    private:
+        static const uint16_t ADR_START = 0x200;
+        static const uint8_t ADR_FONT = 0x50;
+        static const uint8_t SIZE_FONT = 80;
+        static const uint16_t SIZE_SCREEN = 2048;
+        static const uint8_t font[SIZE_FONT];
+
+        typedef enum returnStatus {
+            RS_OK,
+            RS_ROM_TOOBIG,
+            RS_ROM_IOERROR   
+        } returnStatus_t;
+        
+        uint8_t registers[16];
+        uint8_t memory[4096];
+        uint16_t index;
+        uint16_t pc;
+        uint16_t stack[16];
+        uint8_t stackPointer;
+        uint8_t delayTimer;
+        uint8_t soundTimer;
+        uint8_t keypad[16];
+        uint32_t display[SIZE_SCREEN];
+        uint16_t opcode;
+
+        std::default_random_engine randGenerator;
+        std::uniform_int_distribution<uint8_t> randByte;
+
+        returnStatus_t loadROM(const char *filename);
+
+        //Opcodes.
+        void Chip8::OP_00E0(); //CLS
+        void Chip8::OP_00EE(); //RET
+        void Chip8::OP_1NNN(); //JP addr
+        void Chip8::OP_2NNN(); //CALL addr
+        void Chip8::OP_3XKK(); //SE Vx, byte
+        void Chip8::OP_4XKK(); //SNE Vx, byte
+        void Chip8::OP_5XY0(); //SE Vx, Vy
+        void Chip8::OP_6XKK(); //LD Vx, byte
+
+    public:
+        Chip8();
+};
+
+#endif
